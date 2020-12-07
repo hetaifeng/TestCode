1cc1bb2a239d67ddcf3ce79adcd33468ae46ce96
hetaifeng <taifeng.he@saj-electric.com>
2020-12-07 09:10:02 +0800

    test

---
+++ TestCode/Classes/Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  SAJ
+//
+//  Created by SAJ on 2020/12/4.
+//  Copyright © 2020 Guangzhou Sanjing Electric CO., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+- (void)test;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestCode/Classes/Test.m
@@ -0,0 +1,15 @@
+//
+//  Test.m
+//  SAJ
+//
+//  Created by SAJ on 2020/12/4.
+//  Copyright © 2020 Guangzhou Sanjing Electric CO., Ltd. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+- (void)test {
+    NSLog(@"%s",__func__);
+}
+@end
